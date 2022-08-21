4ac811190710c6d08a5ff6c1cba4c6430c92440a
Ronaldo Edicassio de Siqueira <ronaldo.edicassio@gmail.com>
2022-08-20 22:57:38 -0300

    Entity implementation

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,77 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "CPF", nullable = false, unique = true)
+	private String cpf;
+
+	@Column(name = "Id_Departament", nullable = false)
+	private Long departmentId;
+
+	@ManyToOne
+	@JoinColumn(name = "Id_Departament", nullable = false, insertable = false, updatable = false)
+	private Departament departament;
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public Professor() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+}
