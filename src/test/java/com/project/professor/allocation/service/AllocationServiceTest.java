113a75fe2d49ddf6200d3bf6fef02aaece091b96
Ronaldo Edicassio de Siqueira <ronaldo.edicassio@gmail.com>
2022-08-30 07:46:51 -0300

    Allocation test

---
+++ src/test/java/com/project/professor/allocation/service/AllocationServiceTest.java
@@ -0,0 +1,29 @@
+package com.project.professor.allocation.service;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professor.allocation.entity.Allocation;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
+public class AllocationServiceTest {
+
+	SimpleDateFormat sdf = new SimpleDateFormat("HH:mmZ");
+
+	@Autowired
+	AllocationService allocationService;
+
+	@Test
+	public void findAll() {
+		List<Allocation> allocations = allocationService.findAll();
+
+		allocations.forEach(System.out::println);
+	}
+
+}
